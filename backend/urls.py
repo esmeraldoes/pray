34029dfe930b754e9a50ab95e2ae6f90ab280341
esmeraldoes <umoh.esmeraldoes@gmail.com>
2023-07-09 10:56:48 +0100

    shadow5

--- backend/urls.py
+++ backend/urls.py
@@ -1,17 +1,40 @@
-# project/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
+# Create a schema view for Swagger documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API Title",
+        default_version='v1',
+        description="API documentation for Your API",
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
+urlpatterns = [
+    path('', lambda request: redirect('swagger-ui'), name='root'),  # Redirect root URL to Swagger documentation
+    path('admin/', admin.site.urls),
+    path('', include('bibleApp.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
+]
+
+# Add static files serving for development
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('bibleApp.urls')),
-    # path('accounts/', include('allauth.urls')),
-    
-    #path('api/', include((router.urls, 'rest_framework'), namespace='rest_framework')),  # Add this line
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
